b6d25a37a8723e51d700506809963e4eec4e7e13
Lary <110429721+Laryk96@users.noreply.github.com>
2022-08-26 00:16:33 +0300

    Initial commit

---
+++ js/modal.js
@@ -0,0 +1,13 @@
+const refs = {
+  openModalBtn: document.querySelector("[data-open-modal]"),
+  closeModalBtn: document.querySelector("[data-close-modal]"),
+  modal: document.querySelector("[data-backdrop]"),
+};
+
+refs.openModalBtn.addEventListener("click", toggleModal);
+refs.closeModalBtn.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  refs.modal.classList.toggle("is-hidden");
+}
+
